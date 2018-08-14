6560f10e47e366cdd84bc10b4be75c0a55123cf7
Sergei Grechanik <grechanik.sergey@huawei.com>
2018-08-14 19:34:22 +0300

    [TVM] Automatic differentiation for tensor expressions

--- python/tvm/__init__.py
+++ python/tvm/__init__.py
@@ -20,6 +20,7 @@ from . import generic
 from . import hybrid
 from . import testing
 from . import error
+from . import autodiff
 
 from . import ndarray as nd
 from .ndarray import context, cpu, gpu, opencl, cl, vulkan, metal, mtl
@@ -37,6 +38,7 @@ from .ndarray import register_extension
 from .schedule import create_schedule
 from .build_module import build, lower, build_config
 from .tag import tag_scope
+from .autodiff import differentiate
 
 # Contrib initializers
 from .contrib import rocm as _rocm, nvcc as _nvcc, sdaccel as _sdaccel
